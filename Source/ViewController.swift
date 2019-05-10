6ad5c5d7b314741164110aea4b709f020d1bb1b4
christianampe <christianampe.work@gmail.com>
2019-05-10 16:42:36 -0700

    Initial Commit

---
+++ Source/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Christian Ampe on 5/10/19.
+//  Copyright © 2019 christianampe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
