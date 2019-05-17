f994faf7ef83ca1191d0d516b5b5d729050c008d
christianampe <christianampe.work@gmail.com>
2019-05-17 10:10:05 -0700

    finalized styling with a commit to master

--- Source/Application/Home/Collection/Views/UINestedCollectionViewRowCell.swift
+++ Source/Application/Home/Collection/Views/UINestedCollectionViewRowCell.swift
@@ -13,18 +13,10 @@ class UINestedCollectionViewRowCell: UICollectionViewCell {
     /// The `UILabel` controlling the main text of this cell.
     @IBOutlet private weak var title: UILabel!
     
-    /// The `UILabel` controlling the detail text of this cell.
-    @IBOutlet private weak var detail: UILabel!
-    
-    /// The rounded `UIImageView` controlling the icon of this cell.
-    @IBOutlet private weak var icon: UIRoundImageView!
-    
     /// The `UImageView` filling the entire background of this cell.
     @IBOutlet private weak var background: UIImageView!
     
     // MARK: Constraints
-    @IBOutlet weak var contentHeight: NSLayoutConstraint!
-    @IBOutlet weak var contentWidth: NSLayoutConstraint!
     @IBOutlet weak var contentLeadingSpace: NSLayoutConstraint!
     @IBOutlet weak var contentBottomSpace: NSLayoutConstraint!
 }
@@ -37,7 +29,6 @@ extension UINestedCollectionViewRowCell {
     /// - Parameter viewModel: The view model used to populate the UI elements.
     func set(properties viewModel: UINestedCollectionViewRowCellViewModelProtocol) {
         title.text = viewModel.name
-        detail.text = viewModel.detail
         
         guard let url = URL(string: viewModel.backgroundURLString) else {
             return
@@ -64,10 +55,7 @@ private extension UINestedCollectionViewRowCell {
     
     func setContentConstraints() {
         let cellHeight = layer.bounds.size.height
-        let cellWidth = layer.bounds.size.width
         
-        contentHeight.constant = cellHeight * 0.4
-        contentWidth.constant = cellWidth * 0.8
         contentLeadingSpace.constant = cellHeight * 0.1
         contentBottomSpace.constant = cellHeight * 0.1
     }
